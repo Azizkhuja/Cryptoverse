045a6f2463933c6081630c465e335cce57d7edab
Aziz <aziz.khadjayev070@gmail.com>
2021-10-02 16:43:17 +0200

    added card components with single item

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { Cryptocurrencies, News } from "../components";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
@@ -35,6 +36,24 @@ const Homepage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocrueencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
