040d6c5a3a862145ad6bc1a43fd0f3d45377dc54
Aziz <aziz.khadjayev070@gmail.com>
2021-10-02 14:46:18 +0200

    Adding starting file

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "5275a8e1dcmshe6cde61534690eap13333ejsncffe8ac2d285"
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = url => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins")
+    })
+  })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
